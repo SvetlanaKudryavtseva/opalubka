316ff8a7c4e7416d41084dfc7c0b12297737ba20
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-11-08 15:52:06 +0300

    articles in catalog sections

--- [18495] реализация ТЗ/[83] вам будет интересно/catalog/main_seonik/section.php
+++ [18495] реализация ТЗ/[83] вам будет интересно/catalog/main_seonik/section.php
@@ -315,6 +315,105 @@ $bMobileItemsCompact = $arTheme['MOBILE_LIST_ELEMENTS_COMPACT_IN_SECTIONS']['VAL
 				<?=($arParams['MESSAGE_404'] ?:Loc::getMessage("NOT_FOUNDED_SECTION"));?>
 			</div>
 		<?endif;?>
+		
+		<? // Блок "Вам будет интересно" ?> 
+		<?
+			// id инфоблока, в котором будем выводить статьи
+			$IBLOCK_ID = $arParams['IBLOCK_ID'];
+			/*print_r($IBLOCK_ID);*/
+
+			// Получаем массив id-шников элементов, выбранных в пользовательском поле "Статьи в каталоге" в конкретном разделе
+			$rsSelectedItems = CIBlockSection::GetList(
+			["SORT"=>"ASC"],
+			["IBLOCK_ID"=>$IBLOCK_ID,"ID" =>$arSection["ID"]],
+			false,
+			["UF_ARTICLES_IN_CATALOG"],
+		  );
+			while($arSelectedArticles = $rsSelectedItems->GetNext()){
+				$arSelectedIDs = $arSelectedArticles["UF_ARTICLES_IN_CATALOG"];
+				/*print_r($arSelectedIDs);*/
+
+
+			if(!empty($arSelectedIDs)){
+				/*print_r($arSelectedIDs);*/
+				$GLOBALS["arFilterArticlesInCatalog"] = array("ID"=>$arSelectedIDs);
+		?>
+
+		<? } /*end if*/ ?>
+		<? } /*end while*/?>
+
+		<?
+
+		if(!empty($arSelectedIDs)){?>
+		<?$APPLICATION->IncludeComponent(
+			"bitrix:news.list",
+			"blog-list-seonik",
+			Array(
+				"ACTIVE_DATE_FORMAT" => "j F Y",
+				"ADD_SECTIONS_CHAIN" => "Y",
+				"AJAX_MODE" => "N",
+				"AJAX_OPTION_ADDITIONAL" => "",
+				"AJAX_OPTION_HISTORY" => "N",
+				"AJAX_OPTION_JUMP" => "N",
+				"AJAX_OPTION_STYLE" => "Y",
+				"CACHE_FILTER" => "Y",
+				"CACHE_GROUPS" => "Y",
+				"CACHE_TIME" => "36000000",
+				"CACHE_TYPE" => "A",
+				"CHECK_DATES" => "Y",
+				"COMPONENT_TEMPLATE" => "blog-list-seonik",
+				"DETAIL_URL" => "/articles/#SECTION_CODE_PATH#/#ELEMENT_CODE#/",
+				"DISPLAY_BOTTOM_PAGER" => "Y",
+				"DISPLAY_DATE" => "Y",
+				"DISPLAY_NAME" => "Y",
+				"DISPLAY_PICTURE" => "Y",
+				"DISPLAY_PREVIEW_TEXT" => "Y",
+				"DISPLAY_TOP_PAGER" => "N",
+				"FIELD_CODE" => array(0=>"NAME",1=>"PREVIEW_TEXT",2=>"PREVIEW_PICTURE",3=>"DATE_ACTIVE_FROM",4=>"",),
+				"FILTER_NAME" => "arFilterArticlesInCatalog",
+				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+				"IBLOCK_ID" => "37",
+				"IBLOCK_TYPE" => "aspro_allcorp3_content",
+				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+				"INCLUDE_SUBSECTIONS" => "Y",
+				"MESSAGE_404" => "",
+				"NEWS_COUNT" => "4",
+				"PAGER_BASE_LINK_ENABLE" => "N",
+				"PAGER_DESC_NUMBERING" => "N",
+				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+				"PAGER_SHOW_ALL" => "N",
+				"PAGER_SHOW_ALWAYS" => "N",
+				"PAGER_TEMPLATE" => "ajax",
+				"PAGER_TITLE" => "",
+				"PARENT_SECTION" => "",
+				"PARENT_SECTION_CODE" => "",
+				"PREVIEW_TRUNCATE_LEN" => "",
+				"PROPERTY_CODE" => array(0=>"REDIRECT",1=>"PERIOD",2=>"SALE_NUMBER",3=>"",),
+				"RIGHT_LINK" => "articles/",
+				"RIGHT_TITLE" => "Все статьи",
+				"SET_BROWSER_TITLE" => "N",
+				"SET_LAST_MODIFIED" => "N",
+				"SET_META_DESCRIPTION" => "N",
+				"SET_META_KEYWORDS" => "N",
+				"SET_STATUS_404" => "N",
+				"SET_TITLE" => "N",
+				"SHOW_404" => "N",
+				"SHOW_PREVIEW_TEXT" => "Y",
+				"SORT_BY1" => "ACTIVE_FROM",
+				"SORT_BY2" => "SORT",
+				"SORT_ORDER1" => "DESC",
+				"SORT_ORDER2" => "ASC",
+				"STRICT_SECTION_CHECK" => "N",
+				"SUBTITLE" => "",
+				"TITLE" => "Вам будет интересно",
+				"USE_FILTER" => "Y"
+			)
+		);
+
+		}?>
+		<? // end Блок "Вам будет интересно" ?>
+
+	</div>
 	</div>
 	<?if($bShowLeftBlock):?>
 		<?CAllcorp3::ShowPageType('left_block');?>
